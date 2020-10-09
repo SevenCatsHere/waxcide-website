48810b92a96e8d2bb426a3a086ee0975e909df22
Matt Palmer <mspalmer91@gmail.com>
2020-10-08 21:22:19 -0400

    initial layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Layout from '../components/Layout';
 
 export default function Home() {
 	return (
-		<div>
-			Hello world!
-		</div>
+		<Layout>
+			<section />
+		</Layout>
 	);
 }
